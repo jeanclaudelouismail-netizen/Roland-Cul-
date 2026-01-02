d145fa293ec0cd7cc2b2838c98b5ad48f505b7ee
jeanclaudelouismail-netizen <jeanclaude.louis.mail@gmail.com>
2026-01-03 00:39:52 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Roland Culé", page_icon="⚽")
+
+st.title("Roland Culé")
+st.write("Bienvenue dans l'application Streamlit !")
+
+st.subheader("🚀 Application en construction")
+st.write("Ton app est bien connectée à Streamlit. Tu peux maintenant ajouter du contenu ici.")
